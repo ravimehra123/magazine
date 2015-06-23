65614722fc3d842d2380cfd7f5db39936d6880db
ravi mehra <ravi.lib17@gmail.com>
2015-06-23 10:03:50 +0530

    new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magazine::Application.config.secret_key_base = '2a5c34cda084354acc7a60063cc0d8f6cf9c461e41dc7386e17ecd0bb2d3fff29892ddaaa96759e5f5f4aa557ad8e283dcd26ecd71e26fb1807448e74df49b69'
---
+++ db/migrate/20150621052233_add_user_id_to_comments.rb
@@ -0,0 +1,7 @@
+class AddUserIdToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :user_id, :integer
+    add_column :comments, :article_id, :integer
+
+  end
+end
